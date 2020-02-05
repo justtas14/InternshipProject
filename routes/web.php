ca27bfeefea161b6fa32fe75b0ec5bdea74895b1
justt <justtas14@gmail.com>
2020-02-05 09:25:14 +0200

    laravel built-in authentication and bootstrap scaffold

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
